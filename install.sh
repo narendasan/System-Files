b626753feb72cb08a0d376031c748d94c9ad4407
Naren Dasan <naren@narendasan.com>
2015-08-04 10:40:45 -0700

    making cross platform

---
+++ install.sh
@@ -0,0 +1,5 @@
+ln -s ~/system-files/vim ~/.vim
+ln -s ~/system-files/vim/vimrc ~/.vimrc
+ln -s ~/system-files/bash/bashrc ~/.bashrc
+ln -s ~/system-files/bash/aliases ~/.bash_aliases
+ln -s ~/system-files/bash/profile ~/.profile
---
+++ linux_system_recovery.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get distro-upgrade
+
+sudo apt-get install firefox
+sudo apt-get install 
